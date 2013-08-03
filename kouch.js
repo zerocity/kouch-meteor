913a6d64f82b395ed4d2d66f1351c6340aa3bd52
Philpp Bisson <philipp.bisson@gmail.com>
2013-08-03 03:37:25 +0200

    working on meteor

--- kouch.js
+++ kouch.js
@@ -1,18 +1,48 @@
 if (Meteor.isClient) {
   
-  Meteor.call('youtubeQuery','test', function(error, result){
+/*  Meteor.call('youtubeQuery',Session.get('query'), function(error, result){
     Session.set('q', result);
-  });
+  });*/
   
   Template.hello.world = function () {
     return Session.get('q');
   };
 
   Template.hello.events({
-    'click input' : function () {
-      if (typeof console !== 'undefined')
-        console.log(this);
-        Session.set("query",this);
+    'click #search' : function (data) {
+        var query = $('#query').val();
+        console.log('### QUERY: '+query);
+        check(query, String);
+        var max_videos = 12;
+        Meteor.http.call("GET", "http://gdata.youtube.com/feeds/api/videos",
+          {params: {q: query,'max-results':max_videos,alt:'json'}}, function(error,result) {
+            var simpleJson = JSON.parse(result.content).feed.entry ;
+            var data = _.map(simpleJson, function (num,key){
+              var a = num.id.$t.split("/"),
+                id = a[6],
+                title = num.title.$t,
+                thumbnail = num.media$group.media$thumbnail[0].url,
+                description = num.media$group.media$description.$t,
+                totalSec = num.media$group.yt$duration.seconds,
+                hours = parseInt( totalSec / 3600 ) % 24,
+                minutes = parseInt( totalSec / 60 ) % 60,
+                seconds = totalSec % 60,
+                duration = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
+                     
+                return {
+                  id:id,
+                  title:title,
+                  thumbnail:thumbnail,
+                  description:description,
+                  duration:duration};
+                });
+            Session.set("q",data);
+            return true
+          });
+    },
+    'click .play' : function(data){
+      Meteor.call('parseWeb',this.id);
+      console.log('### PLAY: '+ this.title);
     }
   });
 }
@@ -23,7 +53,8 @@ if (Meteor.isServer) {
 
     Meteor.methods({
       youtubeQuery: function(query){
-      console.log('###Youtube\n');
+        //is not nessesary
+      console.log('### '+query);
       check(query, String);
       this.unblock();
       var max_videos = 12;
@@ -59,21 +90,8 @@ if (Meteor.isServer) {
         }
       //parseWeb('http://www.youtube.com/watch?v=duezioB0mxc')
       //Meteor.call('parse','http://www.youtube.com/watch?v=duezioB0mxc');
-      }
-    });
-
-      var player = function(sourceUrl) {
-          console.log('###Player');
-          var player = cp.spawn('mplayer',['-slave','-cache','4096','-fs',sourceUrl.trim()]);
-          //var player = cp.spawn('omxplayer',[sourceUrl.trim()]);16384
-          console.log('### START PLAYER ###');
-          player.stdout.on('data', function (data) {
-            // send commands
-            //mplayer.stdin.write('\nmute')
-          });
-      };
-    
-      var parseWeb = function(sourceUrl) {
+      },
+      parseWeb : function(sourceUrl) {
         console.log('###Parse');
         cp.exec('youtube-dl -g -f 34/35/45/84 '+sourceUrl,function (error, stdout, stderr,stdin) {
           // -f choise prefeard video format http://en.wikipedia.org/wiki/YouTube#Quality_and_codecs
@@ -87,6 +105,18 @@ if (Meteor.isServer) {
             player(stdout);
          };
        });
+      }
+    });
+
+      var player = function(sourceUrl) {
+          console.log('###Player');
+          var player = cp.spawn('mplayer',['-slave','-cache','4096','-fs',sourceUrl.trim()]);
+          //var player = cp.spawn('omxplayer',[sourceUrl.trim()]);16384
+          console.log('### START PLAYER ###');
+          player.stdout.on('data', function (data) {
+            // send commands
+            //mplayer.stdin.write('\nmute')
+          });
       };
 
   });
