33757da3ed351ba95ab84cc003e87a2dfcc9596e
Philpp Bisson <philipp.bisson@gmail.com>
2013-10-27 23:11:19 +0100

    add server ip

--- arch.md
+++ arch.md
@@ -1,1 +1,2 @@
 1. export LC_ALL=C
+2. ?? maybe it works .... sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ Meteor.startup(function () {
   cp = Npm.require('child_process');
   //os = Npm.require('os');
   Fiber = Npm.require("fibers");
-
+  var os=Npm.require('os');
   var fs = Npm.require('fs');
   var path = Npm.require('path');
   var base = path.resolve('../../../../../');
@@ -27,6 +27,23 @@ Meteor.startup(function () {
     ]
   });
 
-playerState.ip = '10.20.30.51'//os.networkInterfaces()['wlan0'][0]['address'];
+
+
+var ifaces=os.networkInterfaces();
+for (var dev in ifaces) {
+  var alias=0;
+  ifaces[dev].forEach(function(details){
+    if (details.family=='IPv4') {
+      console.log(dev+(alias?':'+alias:''),details.address);
+      if (dev == 'eth0' || dev == 'wlan0'){
+        playerState.ip = details.address;
+        console.log('kouch on: ',details.address);
+      }
+      ++alias;
+    }
+  });
+}
+
+//playerState.ip = '10.20.30.51'//os.networkInterfaces()['wlan0'][0]['address'];
 
 });
