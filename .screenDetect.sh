b5d29cf8531ccd55339f5e33b46d11f267e4b545
Philpp Bisson <philipp.bisson@gmail.com>
2013-10-28 00:46:38 +0100

    change screenbalalal

--- .screenDetect.sh
+++ .screenDetect.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # edit according to your laptop and monitor specifications
-LAPTOP_OUTPUT=LVDS
-MONITOR_OUTPUT=VGA-0
-MONITOR_OUTPUT_MODE=1280x1024
+LAPTOP_OUTPUT=LVDS1
+MONITOR_OUTPUT=VGA1
+MONITOR_OUTPUT_MODE=--auto
 MONITOR_OUTPUT_RATE=60
 MODE=`xrandr | grep "$MONITOR_OUTPUT connected" -c`
 if [ "$MODE" == '1' ]
    then
    # external monitor connected
-   xrandr --output MONITOR_OUTPUT --primary && xrandr --output LAPTOP_OUTPUT --off && xrandr --output MONITOR_OUTPUT --mode MONITOR_OUTPUT_MODE --rate MONITOR_OUTPUT_RATE
+   xrandr --output MONITOR_OUTPUT --primary && xrandr --output LAPTOP_OUTPUT --off && xrandr --output MONITOR_OUTPUT --auto
    else
    # external monitor not connected, default for the laptop's monitor
    xrandr --output LAPTOP_OUTPUT --auto
